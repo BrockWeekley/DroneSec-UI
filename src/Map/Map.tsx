98f8f49e23d61c991f1449e08af129006af3c90f
unknown <brockweekley@gmail.com>
2021-04-21 22:30:25 -0500

    Added drone window

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -4,42 +4,43 @@ import { Missouri } from './Missouri';
 import {GeoJSON} from 'geojson';
 import droneImg from '../assets/drone-svg.svg';
 import './Map.css';
+import {useState} from 'react';
 
 const drones: any = [
     {
         id: 1,
         latitude: 38.9517,
-        longitude: -92.3341,
-        displayInfo: false
+        longitude: -92.3341
     },
     {
         id: 2,
         latitude: 39.1231,
-        longitude: -93.1969,
-        displayInfo: false
+        longitude: -93.1969
     },
     {
         id: 3,
         latitude: 37.9485,
-        longitude: -91.7715,
-        displayInfo: false
+        longitude: -91.7715
     },
     {
         id: 4,
         latitude: 40.3461,
-        longitude: -94.8725,
-        displayInfo: false
+        longitude: -94.8725
     },
     {
         id: 5,
         latitude: 38.6270,
-        longitude: -90.1994,
-        displayInfo: false
+        longitude: -90.1994
     }
 ];
 
 const Map: React.FC = () => {
 
+    const [droneDisplay, setDroneDisplay] = useState({
+        id: 1,
+        latitude: 38.9517,
+        longitude: -92.3341
+    });
     const [viewport, setViewport] = React.useState({
         latitude: 37.9643,
         longitude: -91.8318,
@@ -61,8 +62,7 @@ const Map: React.FC = () => {
     const droneMarkers = React.useMemo(() => drones.map(
         (drone: any) => (
             <Marker latitude={drone.latitude} longitude={drone.longitude} key={drone.id}>
-                <img onClick={() => {drone.displayInfo = !drone.displayInfo}} className="drone-icon"  src={droneImg} alt="drone" />
-                {drone.displayInfo && <div className="drone-info">Hello Drone</div>}
+                <img onClick={() => {setDroneDisplay(drone)}} className="drone-icon"  src={droneImg} alt="drone" />
             </Marker>
         ),
     ), []);
@@ -75,6 +75,11 @@ const Map: React.FC = () => {
             height="100%"
             onViewportChange={(viewport: React.SetStateAction<{ latitude: number; longitude: number; zoom: number; }>) => setViewport(viewport)}
         >
+            <div className="drone-info">
+                Selected Drone: {droneDisplay.id}<br />
+                Latitude: {droneDisplay.latitude}<br />
+                Longitude: {droneDisplay.longitude}
+            </div>
             {droneMarkers.length > 0 && droneMarkers}
             <Source id="missouri" type="geojson" data={geojson}>
                 <Layer {...stateLayer} />
