d57ef7d31df96f701d47fa29953e1669254fde1f
Jingsong Tan <jasontanjingsong@gmail.com>
2021-04-24 18:05:08 -0500

    packets style fixes

--- src/PacketBar/PacketBar.tsx
+++ src/PacketBar/PacketBar.tsx
@@ -3,25 +3,25 @@ import MockData from '../assets/data/MOCK_DATA.json';
 import './PacketBar.css'
 
 interface Packet {
-    id: number;
-    packet_dest: string;
-    latency: number;
+  id: number;
+  packet_dest: string;
+  latency: number;
 }
 
 const PacketBar: React.FC = () => {
-      
-      const packet_list = MockData.map((packet: Packet) =>
-        <p className="packet"  key={packet.id}>[{packet.id}]: {packet.packet_dest}   Latency: {packet.latency}</p>
-      );
 
-    return(
-        <div style={{"width": "25%", "overflowY": "auto"}}>
-            <div className="title"><p>PacketBar Component</p></div>
-            <div className="packets">
-            {packet_list}
-            </div>
-        </div>
-    );
+  const packet_list = MockData.map((packet: Packet) =>
+    <p className="packet" key={packet.id}>[{packet.id}]: {packet.packet_dest} &nbsp;&nbsp;&nbsp;&nbsp; Latency: {packet.latency}</p>
+  );
+
+  return (
+    <div className="packet-bar-component">
+      <div className="title"><h4>Packets</h4></div>
+      <div className="packets">
+        {packet_list}
+      </div>
+    </div>
+  );
 };
 
 export default PacketBar;
