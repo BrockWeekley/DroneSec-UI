8328d31fe6120ba066a30a5dc6b470e47afbff3d
James Tompkins <jgt7cn@umsystem.edu>
2021-05-04 21:46:14 -0500

    added first 23 experiments to mock data

--- src/TimeSlider/TimeSlider.tsx
+++ src/TimeSlider/TimeSlider.tsx
@@ -21,6 +21,24 @@ const TimeSlider: React.FC<{onChangeValue:any}> = (props) => {
             <option value="2">Experiment 3</option>
             <option value="3">Experiment 4</option>
             <option value="4">Experiment 5</option>
+            <option value="5">Experiment 6</option>
+            <option value="6">Experiment 7</option>
+            <option value="7">Experiment 8</option>
+            <option value="8">Experiment 9</option>
+            <option value="9">Experiment 10</option>
+            <option value="10">Experiment 11</option>
+            <option value="11">Experiment 12</option>
+            <option value="12">Experiment 13</option>
+            <option value="13">Experiment 14</option>
+            <option value="14">Experiment 15</option>
+            <option value="15">Experiment 16</option>
+            <option value="16">Experiment 17</option>
+            <option value="17">Experiment 18</option>
+            <option value="18">Experiment 19</option>
+            <option value="19">Experiment 20</option>
+            <option value="20">Experiment 21</option>
+            <option value="21">Experiment 22</option>
+            <option value="22">Experiment 23</option>
         </select>
         <div className="select-icon">
         <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" className="icon">
